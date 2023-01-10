71d42cc8714964f9105311d4f851790c7350bea2
nhatty7 <nayopia777@gmail.com>
2023-01-10 09:45:02 -0800

    Commit tasks

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-"""to_json_string
+"""save_to_json_file
 """
 import json
 
-def to_json_string(my_obj):
-    """Return JSON format of my_obj
+
+def save_to_json_file(my_obj, filename):
+    """Writes an object to a text file, using JSON
     """
 
-    return json.dumps(my_obj)
+    with open(filename, mode="w", encoding="UTF-8") as saveFile:
+        json.dump(my_obj, saveFile)
 
